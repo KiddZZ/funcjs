0dfb8bb2003caaeb27a83ae87cba2dfe56486699
kiddzz <zhukaida@hzzhebei.com>
2019-09-27 15:20:23 +0800

    deepClone

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// var deepClone = require("./deep-clone");
-
-// exports.deepClone = deepClone;
-
+var deepClone = require("./deep-clone");
 function geta() {
   console.log("aaaaaaaaaaaaa");
 }
+
+exports.deepClone = deepClone;
 
 exports.geta = geta;
